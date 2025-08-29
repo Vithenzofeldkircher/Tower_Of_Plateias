ee5da9a062c36dc1fe4dd5c2cb651d06b6dde157
molesty <thiago.matos1@alunos.sc.senac.br>
2025-08-29 11:40:24 -0300

    historia

---
+++ Assets/Scripts/MudarHistoria1.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MudarHistoria1 : MonoBehaviour
+{   
+        public void LoadScene()
+        {
+            SceneManager.LoadScene("Historia2");
+        }
+
+}
